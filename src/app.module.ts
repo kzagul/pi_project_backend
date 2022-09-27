3fd9f0cb6dae496a11eca73d76fe24eb0c54e17b
Kirill Zagul <zagulkirill@gmail.com>
2022-09-27 18:37:25 +0500

    Инициализация контроллеров: meeting, client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MeetingController } from './controllers/meeting/meeting.controller';
+import { ClientController } from './controllers/client/client.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MeetingController, ClientController],
   providers: [AppService],
 })
 export class AppModule {}
